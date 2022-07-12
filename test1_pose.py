ca6fa8fe7fd9f7fa99c1e5b018f13107e061e10b
y0ngma <takeit2sy@pusan.ac.kr>
2022-07-12 11:32:04 +0900

    기능. 추가 사람외 배경 칠하기

--- test1_pose.py
+++ test1_pose.py
@@ -1,10 +1,10 @@
 import cv2
-# import mediapipe as mp
+import urllib.request
+from PIL import Image
+import numpy as np
 from mediapipe.python.solutions import drawing_utils as mputils
 from mediapipe.python.solutions import drawing_styles as mpstyles
 from mediapipe.python.solutions import pose as mppose
-import urllib.request
-from PIL import Image
 
 img_url = "https://static.turbosquid.com/Preview/2015/11/10__11_56_36/anthonystanding23dmetry3dhuman01.jpg5e774d4d-9b9e-456d-9d7b-fc4d741cf940Large.jpg"
 file = 'pose.jpg'
@@ -26,4 +26,22 @@ mputils.draw_landmarks(annotated_image,
 
 filename = "pose_wireframe.png"
 cv2.imwrite(filename, annotated_image)
+# Image.open(filename).show()
+
+# pose estimation된 객체에 속할 확률임계치를 넘은 픽셀 빼고 나머지를 배경색으로 칠하기
+segmented_image = image.copy()
+seg_thres = .3 # 낮을땐 덜 칠해지는 부분이 많은것 확인 가능
+
+# segmentation mask를 3 RGB채널로 stack후 어느픽셀을 유지할지 필터 생성
+condition = np.stack((results.segmentation_mask,)*3, axis=-1) > seg_thres
+
+# 칠할 검은배경 생성
+bg_image = np.zeros(image.shape, dtype=np.uint8)
+# bg_image[:] = [255,0,0] # 다른색으로 칠하기 가능
+
+# if condition: segmented_image else bg_image
+segmented_image = np.where(condition, segmented_image, bg_image)
+
+filename = "pose_segmentation.png"
+cv2.imwrite(filename, segmented_image)
 Image.open(filename).show()
